2f1d86295ad30a6cd3c00706bb2b94037cef30aa
WeAreNotEternal <99015955+WeAreNotEternal@users.noreply.github.com>
2022-02-14 10:38:56 +0700

    Update Chess.c

--- Chess.c
+++ Chess.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
- int main(){ 
+int main()
+{ 
 printf("  A B C D E F G H \n"); 
 printf("8 R N B Q K B N R 8 \n"); 
 printf("7 P P P P P P P P 7 \n"); 
@@ -9,4 +10,4 @@ printf("3                 3 \n");
 printf("2 P P P P P P P P 2 \n"); 
 printf("1 R N B Q K B N R 1 \n"); 
 printf("  A B C D E F G H \n"); 
- }
+}
